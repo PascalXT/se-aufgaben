e46fba663177170fa8b40347f46f72d9bdca85d3
Pascal <spaceranger21@aol.com>
2011-01-05 12:02:07 +0100

    Implemented first version of Q7.

--- Datenbanken/WahlWebsite/src/queries/Q7.java
+++ Datenbanken/WahlWebsite/src/queries/Q7.java
@@ -3,23 +3,43 @@ package queries;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Q7 extends Query {
+import database.DB;
 
-	public Q7(String headline) {
-		super(headline);
-		// TODO Auto-generated constructor stub
+public class Q7 extends Q3 {
+	public Q7(String headline, int wahlkreisID) {
+		super(headline, wahlkreisID);
 	}
 
+	final String kTempZweitStimmenNachWahlkreis = db.tabellenName("TempZweitStimmenNachWahlkreis");
+	final String kTempErstStimmenNachWahlkreis = db.tabellenName("kTempErstStimmenNachWahlkreis");
+	
+	protected String createTempZweitStimmenNachWahlkreisTable() throws SQLException {
+	 	db.createOrReplaceTemporaryTable(kTempZweitStimmenNachWahlkreis, DB.kForeignKeyParteiID + " BIGINT, "
+	 			+ DB.kForeignKeyWahlkreisID + " BIGINT, " + DB.kJahr + " BIGINT, " + DB.kAnzahlStimmen + " BIGINT");
+    db.executeUpdate(""
+    		+ "INSERT INTO " + kTempZweitStimmenNachWahlkreis + "(" + DB.kForeignKeyParteiID + ", "
+    			+ DB.kForeignKeyWahlkreisID + ", " + DB.kJahr + ", " + DB.kAnzahlStimmen + ") "
+    		+ "SELECT s." + DB.kForeignKeyParteiID + ", s." + DB.kForeignKeyWahlkreisID + ", " + DB.kJahr + ", COUNT(*) "
+    		+ "FROM " + db.stimme() + " s "
+    		+ "GROUP BY s." + DB.kForeignKeyParteiID + ", s." + DB.kForeignKeyWahlkreisID + ", s." + DB.kJahr);
+    return kTempZweitStimmenNachWahlkreis;
+	}
+	
+	protected String createTempErstStimmenNachWahlkreisTable() throws SQLException {
+	 	db.createOrReplaceTemporaryTable(kTempErstStimmenNachWahlkreis, DB.kForeignKeyKandidatID + " BIGINT, "
+	 			+ DB.kForeignKeyWahlkreisID + " BIGINT, " + DB.kJahr + " BIGINT, " + DB.kAnzahlStimmen + " BIGINT");
+    db.executeUpdate(""
+    		+ "INSERT INTO " + kTempErstStimmenNachWahlkreis + "(" + DB.kForeignKeyKandidatID + ", "
+    			+ DB.kForeignKeyWahlkreisID + ", " + DB.kJahr + ", " + DB.kAnzahlStimmen + ") "
+    		+ "SELECT s." + DB.kForeignKeyKandidatID + ", s." + DB.kForeignKeyWahlkreisID + ", " + DB.kJahr + ", COUNT(*) "
+    		+ "FROM " + db.stimme() + " s "
+    		+ "GROUP BY s." + DB.kForeignKeyKandidatID + ", s." + DB.kForeignKeyWahlkreisID + ", s." + DB.kJahr);
+    return kTempErstStimmenNachWahlkreis;
+	}
+	
 	@Override
 	protected ResultSet doQuery() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		// Aggregate Wahlkreis data.	
+		return doQuery(createTempErstStimmenNachWahlkreisTable(), createTempZweitStimmenNachWahlkreisTable());
 	}
-
-	@Override
-	protected String generateBody(ResultSet resultSet) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
